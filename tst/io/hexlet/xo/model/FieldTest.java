3f23aa315f17f1f2dda96ee0ea6cd688655b6723
Stas <derager@rambler.ru>
2018-09-22 20:19:23 +0700

    add flexible field size

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -13,7 +13,7 @@ public class FieldTest {
     @Test
     public void getSize() {
 
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
@@ -21,7 +21,7 @@ public class FieldTest {
     @Test
     public void setFigure() throws AlreadyOccupiedException, InvalidPointException {
 
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         final Point inputPoint = new Point(0,0);
 
@@ -37,7 +37,7 @@ public class FieldTest {
     @Test
     public void testGetFigureWhenFigureIsNotSet() throws AlreadyOccupiedException, InvalidPointException {
 
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0,0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -48,7 +48,7 @@ public class FieldTest {
     @Test
     public void testGetFigureWhenXIsLessThenZero() throws AlreadyOccupiedException, InvalidPointException {
 
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1,0);
 
         try {
@@ -62,7 +62,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThenZero() throws AlreadyOccupiedException, InvalidPointException {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -1);
 
         try {
@@ -73,7 +73,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsMoreThenSize() throws AlreadyOccupiedException, InvalidPointException {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
 
         try {
@@ -84,7 +84,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThenSize() throws AlreadyOccupiedException, InvalidPointException {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
 
         try {
